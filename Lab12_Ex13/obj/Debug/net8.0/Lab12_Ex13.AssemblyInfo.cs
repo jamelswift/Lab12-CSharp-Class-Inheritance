ba47d02e1aeb18b0d0d886d8eddc529cf1b219ce
jamelswift <Wipatsasicha0702@gmail.com>
2025-04-01 02:11:43 +0700

    66030281 วิภัสศศิชา

--- Lab12_Ex13/obj/Debug/net8.0/Lab12_Ex13.AssemblyInfo.cs
+++ Lab12_Ex13/obj/Debug/net8.0/Lab12_Ex13.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab12_Ex13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f45a0b27d597da273c6bdfd62d490b8719d2242")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c6542e91964c9b6cf09a7ca7d0d4cbfc3caf31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab12_Ex13")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab12_Ex13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
